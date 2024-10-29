55d9aeb29b8564aee6075e66fa4ba8b4a4e4aca3
Paulo Sergio <pauloserg861@gmail.com>
2024-10-28 21:32:47 -0300

    test: ensure a new user is created with default values

--- src/module/user/domain/entities/user.spec.ts
+++ src/module/user/domain/entities/user.spec.ts
@@ -9,4 +9,18 @@ describe('User entity', () => {
     });
     expect(sut).toBeDefined();
   });
+
+  describe('User entity constructor', () => {
+    it('Should create a new user with default values', () => {
+      const sut = new User({
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+      });
+      expect(sut.id).toBeDefined();
+      expect(sut.avatar).toBeNull();
+      expect(sut.isActive).toBeTruthy();
+      expect(sut.createdAt).toBeInstanceOf(Date);
+    });
+  });
 });
