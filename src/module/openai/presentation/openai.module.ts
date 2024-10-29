f500033de36ff1493751263ee1a0a3aa49b5660a
Paulo Sergio <pauloserg861@gmail.com>
2024-10-28 21:08:58 -0300

    feat: provide injectables

--- src/module/openai/presentation/openai.module.ts
+++ src/module/openai/presentation/openai.module.ts
@@ -1,9 +1,12 @@
 import { Global, Module } from '@nestjs/common';
+import { makeOpenaiProvider } from '../infra/providers/openai.provider';
 
+const providers = [...makeOpenaiProvider()];
 @Global()
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers,
+  exports: providers,
 })
 export class OpenaiModule {}
